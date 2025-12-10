13597f40ee906e613b3007fa87553ded8fa11b7f
Emanuele Pio Santangelo <emanuele@MacBookPro.homenet.telecomitalia.it>
2025-12-10 22:39:04 +0100

    [Em] refactor(utility/ImageLLMUtils): Aggiunto metodo speciale di generazione attraverso RAG;

--- utility/imagellm_utils.py
+++ utility/imagellm_utils.py
@@ -4,10 +4,10 @@ from typing import List, Dict
 from PIL import Image
 import torch
 from vllm_utils import VLLMUtils
+from vector_store_utils import VectorStoreUtils
 
 
 class ImageLLMUtils:
-
     def __init__(self):
         self.mode = dotenv_values("../.env")["INFERENCE_MODE"]
         self.precision = torch.bfloat16 if self.mode == "cuda" else torch.float32
@@ -31,21 +31,66 @@ class ImageLLMUtils:
         inf_steps=50,
         gen_images=1,
         output_filename="output_edit",
+        persist=True
     ) -> List[Image.Image]:
         polished_prompt = self.vllm.polish_prompt_en(prompt, images)
         inputs = self._make_inputs(
             images, polished_prompt, guidance, neg_prompt, inf_steps, gen_images
         )
-        return self._generate_image(inputs, output_filename)
+        return self._generate_image(inputs, output_filename, persist=persist)
 
-    def _generate_image(self, inputs: Dict, output_filename) -> List[Image.Image]:
+    def generate_with_rag(
+        self,
+        prompt: str,
+        concepts: List[str],
+        images: List[Image.Image],
+        vectorStore: VectorStoreUtils,
+        guidance=4.0,
+        neg_prompt="",
+        inf_steps=50,
+        gen_images=1,
+        output_filename="output_edit",
+        persist=True,
+    ) -> List[Image.Image]:
+        unknown_concepts = self.vllm.knowledge_eval(concepts=concepts)
+        retrieved = vectorStore.query(unknown_concepts)
+
+        result = self.generate(
+            prompt,
+            images.append(retrieved),
+            guidance=guidance,
+            neg_prompt=neg_prompt,
+            inf_steps=inf_steps,
+            gen_images=gen_images,
+            persist=False
+        )
+
+        missing = self.vllm.inclusion_evaluation(concepts, result)
+        if len(missing["missing"]) == 0:
+            return result
+        else:
+            retrieved = vectorStore.query(missing)
+            result = self.generate(
+                prompt,
+                images.append(retrieved),
+                guidance=guidance,
+                neg_prompt=neg_prompt,
+                inf_steps=inf_steps,
+                gen_images=gen_images,
+                output_filename=output_filename,
+                persist=persist
+            )
+            return result
+
+    def _generate_image(self, inputs: Dict, output_filename, persist=True) -> List[Image.Image]:
         with torch.inference_mode():
             output = self.pipeline(**inputs)
             i = 0
-            for output_image in output.images:
-                output_image.save(f"{output_filename}_{i}.jpeg")
-                i += 1
-            print("Image generated (saved!)")
+            if(persist):
+                for output_image in output.images:
+                    output_image.save(f"{output_filename}_{i}.jpeg")
+                    i += 1
+                print("Image generated (saved!)")
         return output.images
 
     def _make_inputs(
