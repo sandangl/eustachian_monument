46859a7564cdde74141f1e6ed4a54b0bbea1c70e
Emanuele Pio Santangelo <emanuele@MacBook-Pro-di-Emanuele.local>
2025-12-10 10:22:06 +0100

    [Em, St] feat(utility/VectorStoreUtils): aggiunti metodi di encode e decode immagini;

--- utility/vector_store_utils.py
+++ utility/vector_store_utils.py
@@ -1,4 +1,8 @@
 import chromadb
+import base64
+import io
+from matplotlib import image as mpimg
+from matplotlib import pyplot as plt
 from typing import List
 
 class VectorStoreUtils: 
@@ -22,4 +26,15 @@ class VectorStoreUtils:
               entry 
            ]
         )
-        self.lastId += 1      
+        self.lastId += 1    
+
+    def _encode_image(self, image_path) -> str:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    
+    def _decode_and_show_image(self, base64_img, img_format: str):
+        decoded_bytes = io.BytesIO(base64.b64decode(base64_img))
+        decoded_image = mpimg.imread(decoded_bytes, format=img_format)
+        
+        plt.imshow(decoded_image, interpolation='nearest')
+        plt.show()  
